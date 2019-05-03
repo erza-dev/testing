d5f85c69780678deb47c9e7acf6fa04fc6e5e56f
Erza Taufik S <erza.dev@gmail.com>
2019-05-04 01:51:00 +0700

    first commit :yey:

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello World! 
+
+im starting agian :(
---
+++ index.js
@@ -0,0 +1,13 @@
+// object literal
+
+const MyBio= {
+    firstName: 'Erza',
+    lastName: 'Taufik',
+    age: 23,
+    hobbies :['coffee', 'gaming', 'programming'],
+    address : {
+        street : 'Griya Nirmala St. no.5 ',
+        province : 'Lampung'
+    }
+}
+
